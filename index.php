406438b7ef066cec65968d9a40d14427652691cd
jonisiito <97846016+jonisiito@users.noreply.github.com>
2023-03-06 15:18:10 +0100

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ if (isset($_COOKIE['sesion'])) {
     $result = $conexion->query($sql);
         if ($result->num_rows > 0) {
         $sesion=$_COOKIE['sesion'];
-        echo '<form id="myForm" action="http://"'+$_SERVER_NAME['URL']+'"/createcookie" method="post">';
+        echo '<form id="myForm" action="http://"'.$_SERVER_NAME['URL'].'"/createcookie" method="post">';
         echo '<input type="hidden" name="sesion" value="'.$sesion.'">';
         echo '<script type="text/javascript">document.getElementById("myForm").submit();</script>';
         }
