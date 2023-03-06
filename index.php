fe6c9343c55d64b4311a719772769889a13ce850
jonisiito <97846016+jonisiito@users.noreply.github.com>
2023-03-06 14:37:51 +0100

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ if (isset($_COOKIE['sesion'])) {
     $result = $conexion->query($sql);
     if ($result->num_rows > 0) {
         $sesion=$_COOKIE['sesion'];
-        echo '<form id="myForm" action="http://3.237.171.101/createcookie" method="post">';
+        echo '<form id="myForm" action="http://"+$_SERVER_NAME['URL']+"/createcookie" method="post">';
         echo '<input type="hidden" name="sesion" value="'.$sesion.'">';
         echo '<script type="text/javascript">document.getElementById("myForm").submit();</script>';
     }
@@ -55,7 +55,7 @@ if (isset($_COOKIE['sesion'])) {
                     <input id="registrarsee" class="boton" type="submit" value="Iniciar sesion" name="login">
                     <div class="button-container">
                         <a class="salir" href="https://google.es">Salir</a>
-                        <a class="login" href="http://localhost/putojoni/proyectoregistro/registro.php">Registrarse</a>
+                        <a class="login" href="http://"+$_SERVER_NAME['URL']+"">Registrarse</a>
                     </div>
                 </div>
             </div>
